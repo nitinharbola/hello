f65d0b73339a43ce9ea9df89efa35928142ae309
nitinharbola <nitinharbola01@gmail.com>
2014-07-20 21:56:18 +0530

    ok

---
+++ testing/Observer.php
@@ -0,0 +1,21 @@
+<?php
+class SW_GridExample_Model_Observer
+{
+    public function addActions($observer)
+    {
+        $block = $observer->getEvent()->getBlock();
+        // Check if this block is a MassAction block
+        if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction) {
+            // Check if we're dealing with the Orders grid
+            if ($block->getParentBlock() instanceof Mage_Adminhtml_Block_Sales_Order_Grid) {
+                // The first parameter has to be unique, or you'll overwrite the old action.
+                $block->addItem('demo', array(
+                        'label' => Mage::helper('sales')->__('Example'),
+                        'url' => $block->getUrl('*/*/'),
+                    )
+                );
+            }
+        }
+    }
+}
+testing123456789
